9b5feffd2a06e139b10ad80b72a5ed852935cc1c
Pavel Zabalotny <pavelzabalotny@gmail.com>
2025-04-12 21:01:53 +0300

    fix(app): remove unnecessary code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react';
 
 import AppRouter from '~/AppRouter';
-import { useGetPostsQuery } from '~/query/services/posts.ts';
 import customTheme from '~/theme';
 
 function App() {
-    const { data: _data, isLoading: _isLoading } = useGetPostsQuery();
-
     return (
         <ChakraProvider theme={customTheme}>
             <AppRouter />
